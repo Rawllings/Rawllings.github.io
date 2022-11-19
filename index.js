e71532f87912098d4f781b9e39b2e3947e281e8a
Rawllings Kiprop <rutojr01@gmail.com>
2022-11-19 13:31:21 +0300

    Java Script onmouseover applied

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function save(){
     alert("Downloading");
 }
+
+function navbar() {
+document.getElementById('btn').innerHTML = "Download"
+}
+
+function navbar1() {
+    document.getElementById('btn').innerHTML = "Downloading"
+    }
